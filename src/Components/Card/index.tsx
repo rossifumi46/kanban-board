048d72f7b3b740cc3afd5d6d363f519af804ef73
unknown <rossifumi.gp@gmail.com>
2021-06-26 16:53:05 +0600

    init

---
+++ src/Components/Card/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './style';
+
+export default function Card() {
+  return (
+    <Container>
+      <button type="button">Start</button>
+    </Container>
+  );
+}
---
+++ src/style.js
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const Title = styled.div`
+  
+`;
+
+export const Boards = styled.div`
+  display: flex;
+`;
